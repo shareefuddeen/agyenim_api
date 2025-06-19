1379ec04efd96877bcef36cdb07dd61528b2bf6a
shareefuddeen <140364218+shareefuddeen@users.noreply.github.com>
2025-06-19 10:28:45 -1200

    image url fix

--- api/serializers.py
+++ api/serializers.py
@@ -26,6 +26,13 @@ class BlogSerializer(serializers.ModelSerializer):
         
         
 class TeamSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
     class Meta:
         model = models.Team
         fields = "__all__"
+
+    def get_image(self, obj):
+        request = self.context.get('request')
+        if request is not None:
+            return request.build_absolute_uri(obj.image.url)
+        return obj.image.url
